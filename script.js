50626b551dd8bf24733c53ce55f10d77fd1c8067
guilherme-mello-florio <124586654+guilherme-mello-florio@users.noreply.github.com>
2024-04-09 22:08:37 -0300

    Fetch de API
    Fetch de API feito para alunos na página inicial, sem aplicação de filtro

--- script.js
+++ script.js
@@ -31,3 +31,29 @@ function fechaPagAjustes(){
     let pag_ajustes = document.getElementById("background_pag_ajustes");
     pag_ajustes.style.display = "none";
 }
+
+function getAlunos(){
+    fetch('http://localhost:3000/api/alunos')
+	.then(function (resposta) {
+        console.log(resposta);
+		return resposta.json();
+	})
+	.then(function (dados) {
+        console.log(dados);
+        console.log(dados.result.length)
+
+        dados.result.forEach(function(aluno){
+            var alunoDiv = document.createElement('aside');
+            var alunoIdDiv = document.createElement('article');
+            var alunoNomeDiv = document.createElement('article');
+            var alunoFaltasDiv = document.createElement('article');
+            alunoIdDiv.appendChild(document.createTextNode(aluno.id_aluno));
+            alunoNomeDiv.appendChild(document.createTextNode(aluno.nome_aluno));
+            alunoFaltasDiv.appendChild(document.createTextNode(aluno.n_de_faltas));
+            alunoDiv.appendChild(alunoIdDiv);
+            alunoDiv.appendChild(alunoNomeDiv);
+            alunoDiv.appendChild(alunoFaltasDiv);
+            document.getElementById('campo_turma').appendChild(alunoDiv);
+        })
+	})
+}
